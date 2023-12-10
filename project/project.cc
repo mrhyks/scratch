9fa7cda6682142232c49605cd57d7cecbf1bcf53
mrhapkys <matej.hyks@seznam.cz>
2023-12-10 19:27:33 +0100

    Co-authored-by: Jorgemyte <Jorgemyte@users.noreply.github.com> Co-authored-by: tamide <tamide@users.noreply.github.com>

--- project/project.cc
+++ project/project.cc
@@ -7,6 +7,10 @@
 #include "ns3/buildings-propagation-loss-model.h"
 #include "ns3/config-store-module.h"
 #include "ns3/core-module.h"
+#include "ns3/data-rate.h"
+#include "ns3/flow-monitor-helper.h"
+#include "ns3/flow-monitor-module.h"
+#include "ns3/gnuplot.h"
 #include "ns3/internet-module.h"
 #include "ns3/lte-module.h"
 #include "ns3/mobility-module.h"
@@ -15,12 +19,11 @@
 #include "ns3/pcap-file.h" // Include the pcap file header
 #include "ns3/point-to-point-module.h"
 #include "ns3/propagation-loss-model.h"
+#include "ns3/traffic-control-module.h"
 
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("project");
-
-
 
 void
 ServerConnectionEstablished(Ptr<const ThreeGppHttpServer>, Ptr<Socket>)
@@ -100,6 +103,7 @@ main(int argc, char* argv[])
     uint16_t webSocketServer = 1100;
     uint16_t webSocketClient = 2000;
     Time interPacketInterval = MilliSeconds(200);
+    uint16_t numberOfUes = 10;
 
     // Command line arguments
     CommandLine cmd;
@@ -123,7 +127,7 @@ main(int argc, char* argv[])
 
     // Create UEs, it was only for try the model
     NodeContainer ueNodes;
-    ueNodes.Create(10); // number of UEs defined by numNodePairs
+    ueNodes.Create(numberOfUes); // number of UEs defined by numNodePairs
 
     // Install Mobility Model
     MobilityHelper mobility;
@@ -175,6 +179,11 @@ main(int argc, char* argv[])
         lteHelper->InstallUeDevice(ueNodes); // add UE nodes to the container
     Ptr<Node> pgw = epcHelper->GetPgwNode(); // get the PGW node
 
+    // Install Mobility Model for PGW
+    MobilityHelper pgwMobility;
+    pgwMobility.SetMobilityModel("ns3::ConstantPositionMobilityModel"); // mobility model (constant)
+    pgwMobility.Install(pgw);
+
     // Create a single RemoteHost
     NodeContainer remoteHostContainer; // container for remote node
     remoteHostContainer.Create(1);     // create 1 node
@@ -182,6 +191,11 @@ main(int argc, char* argv[])
     InternetStackHelper internet;
     internet.Install(remoteHostContainer); // aggregate stack implementations (ipv4, ipv6, udp, tcp)
                                            // to remote node
+
+    MobilityHelper remoteHostMobility;
+    remoteHostMobility.SetMobilityModel(
+        "ns3::ConstantPositionMobilityModel"); // mobility model (constant)
+    remoteHostMobility.Install(remoteHost);
 
     // Create the Internet
     PointToPointHelper p2ph;
@@ -219,7 +233,7 @@ main(int argc, char* argv[])
     }
 
     // Attach one UE per eNodeB
-    for (uint16_t i = 0; i < numNodePairs; i++)
+    for (uint16_t i = 0; i < numberOfUes; i++)
     {
         if (i % 2 == 0)
         {
@@ -233,6 +247,7 @@ main(int argc, char* argv[])
         }
         // side effect: the default EPS bearer will be activated
     }
+
     // Create HTTP server helper
     ThreeGppHttpServerHelper serverHelper(remoteHostAddr);
 
@@ -261,33 +276,149 @@ main(int argc, char* argv[])
     // Install HTTP client
     for (uint32_t u = 0; u < ueNodes.GetN(); ++u)
     {
-        // Seed the random number generator
-        std::srand(std::time(0));
+        clientApps = clientHelper.Install(ueNodes.Get(u));
+        Ptr<ThreeGppHttpClient> httpClient = clientApps.Get(0)->GetObject<ThreeGppHttpClient>();
 
-        // Generate a random number in the range from 1 to 2
-        int randomNumber = 1 + std::rand() % 2;
-
-        if (randomNumber == 1)
-        {
-            clientApps = clientHelper.Install(ueNodes.Get(u));
-            Ptr<ThreeGppHttpClient> httpClient = clientApps.Get(0)->GetObject<ThreeGppHttpClient>();
-
-            // Example of connecting to the trace sources
-            httpClient->TraceConnectWithoutContext("RxMainObject",
-                                                   MakeCallback(&ClientMainObjectReceived));
-            httpClient->TraceConnectWithoutContext("RxEmbeddedObject",
-                                                   MakeCallback(&ClientEmbeddedObjectReceived));
-            httpClient->TraceConnectWithoutContext("Rx", MakeCallback(&ClientRx));
-        }
+        // Example of connecting to the trace sources
+        httpClient->TraceConnectWithoutContext("RxMainObject",
+                                               MakeCallback(&ClientMainObjectReceived));
+        httpClient->TraceConnectWithoutContext("RxEmbeddedObject",
+                                               MakeCallback(&ClientEmbeddedObjectReceived));
+        httpClient->TraceConnectWithoutContext("Rx", MakeCallback(&ClientRx));
     }
 
     serverApps.Start(MilliSeconds(500));
     clientApps.Start(MilliSeconds(500));
 
+    double startTime = Simulator::Now().GetSeconds();
+    double latencyForOperation = Simulator::Now().GetSeconds() - startTime;
+    NS_LOG_INFO("Latency for the operation: " << latencyForOperation);
+
     // Run simulation
     Simulator::Stop(simTime);
     p2ph.EnablePcapAll("project");
+    Ptr<FlowMonitor> monitor; // = flowMonHelper.InstallAll();
+    FlowMonitorHelper flowMonHelper;
+
+    // NetAnim animation
+    AnimationInterface animation("project.xml"); // 1 Is SGW, 0 is PGW, 3 is Remote Host, 2 is MME,
+                                                 // 6 and 7 are UE, 4 are 5 are eNodeB
+    // animation.UpdateNodeDescription(pgw,"PGW");
+    // animation.UpdateNodeDescription(remoteHost,"Remote Host");
+    // animation.UpdateNodeDescription(1,"SGW");
+    // animation.UpdateNodeDescription(2,"MME");
+
+    // for (uint32_t u = 0; u < ueNodes.GetN (); ++u){
+    //     animation.UpdateNodeDescription(ueNodes.Get (u),"UE_"+std::to_string(u));
+    //     animation.UpdateNodeColor(ueNodes.Get (u), 0, 0, 255);
+    // }
+
+    // for (uint32_t u = 0; u < enbNodes.GetN (); ++u){
+    //     animation.UpdateNodeDescription(enbNodes.Get (u),"eNodeB_"+std::to_string(u));
+    //     animation.UpdateNodeColor(enbNodes.Get (u), 0, 255, 0);
+    // }
+
+    monitor = flowMonHelper.Install(enbNodes);
+    monitor = flowMonHelper.Install(ueNodes);
+    monitor = flowMonHelper.Install(remoteHost);
     Simulator::Run();
+
+    // GnuPlot
+    std::string jmenoSouboru = "project_delay";
+    std::string graphicsFileName = jmenoSouboru + ".png";
+    std::string plotFileName = jmenoSouboru + ".plt";
+    std::string plotTitle = "Average delay";
+    std::string dataTitle = "Delay [ms]";
+    Gnuplot gnuplot(graphicsFileName);
+    gnuplot.SetTitle(plotTitle);
+    gnuplot.SetTerminal("png");
+    gnuplot.SetLegend("IDs of data streams", "Delay [ms]");
+    gnuplot.AppendExtra("set xrange [1:" + std::to_string(numberOfUes * 2) + "]");
+    gnuplot.AppendExtra("set yrange [0:500]");
+    gnuplot.AppendExtra("set grid");
+    Gnuplot2dDataset dataset_delay;
+
+    jmenoSouboru = "project_data_rate";
+    graphicsFileName = jmenoSouboru + ".png";
+    std::string plotFileNameDR = jmenoSouboru + ".plt";
+    plotTitle = "Data rate for IDs";
+    dataTitle = "Data rate [kbps]";
+    Gnuplot gnuplot_DR(graphicsFileName);
+    gnuplot_DR.SetTitle(plotTitle);
+    gnuplot_DR.SetTerminal("png");
+    gnuplot_DR.SetLegend("IDs of all streams", "Data rate [kbps]");
+    gnuplot_DR.AppendExtra("set xrange [1:" + std::to_string(numberOfUes * 2) + "]");
+    gnuplot_DR.AppendExtra("set yrange [0:2500]");
+    gnuplot_DR.AppendExtra("set grid");
+    Gnuplot2dDataset dataset_rate;
+
+    monitor->CheckForLostPackets();
+    Ptr<Ipv4FlowClassifier> classifier =
+        DynamicCast<Ipv4FlowClassifier>(flowMonHelper.GetClassifier());
+    std::map<FlowId, FlowMonitor::FlowStats> stats = monitor->GetFlowStats();
+
+    monitor->SerializeToXmlFile("project.flowmon", true, true);
+
+    std::cout << std::endl << "*** Flow monitor statistic ***" << std::endl;
+    for (std::map<FlowId, FlowMonitor::FlowStats>::const_iterator i = stats.begin();
+         i != stats.end();
+         ++i)
+    {
+        // if (i-> first > 2) {
+        double Delay, DataRate;
+        Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow(i->first);
+        std::cout << "Flow ID: " << i->first << std::endl;
+        std::cout << "Src add: " << t.sourceAddress << "-> Dst add: " << t.destinationAddress
+                  << std::endl;
+        std::cout << "Src port: " << t.sourcePort << "-> Dst port: " << t.destinationPort
+                  << std::endl;
+        std::cout << "Tx Packets/Bytes: " << i->second.txPackets << "/" << i->second.txBytes
+                  << std::endl;
+        std::cout << "Rx Packets/Bytes: " << i->second.rxPackets << "/" << i->second.rxBytes
+                  << std::endl;
+        std::cout << "Throughput: "
+                  << i->second.rxBytes * 8.0 /
+                         (i->second.timeLastRxPacket.GetSeconds() -
+                          i->second.timeFirstTxPacket.GetSeconds()) /
+                         1024
+                  << "kb/s" << std::endl;
+        std::cout << "Delay sum: " << i->second.delaySum.GetMilliSeconds() << "ms" << std::endl;
+        std::cout << "Mean delay: "
+                  << (i->second.delaySum.GetSeconds() / i->second.rxPackets) * 1000 << "ms"
+                  << std::endl;
+
+        // gnuplot Delay
+        Delay = (i->second.delaySum.GetSeconds() / i->second.rxPackets) * 1000;
+        DataRate =
+            i->second.rxBytes * 8.0 /
+            (i->second.timeLastRxPacket.GetSeconds() - i->second.timeFirstTxPacket.GetSeconds()) /
+            1024;
+
+        dataset_delay.Add((double)i->first, (double)Delay);
+        dataset_rate.Add((double)i->first, (double)DataRate);
+        std::cout << "Jitter sum: " << i->second.jitterSum.GetMilliSeconds() << "ms" << std::endl;
+        std::cout << "Mean jitter: "
+                  << (i->second.jitterSum.GetSeconds() / (i->second.rxPackets - 1)) * 1000 << "ms"
+                  << std::endl;
+        // std::cout << "Lost Packets: " << i->second.lostPackets << std::endl;
+        std::cout << "Lost Packets: " << i->second.txPackets - i->second.rxPackets << std::endl;
+        std::cout << "Packet loss: "
+                  << (((i->second.txPackets - i->second.rxPackets) * 1.0) / i->second.txPackets) *
+                         100
+                  << "%" << std::endl;
+        std::cout << "------------------------------------------------" << std::endl;
+    }
+
+    // Gnuplot - continuation
+    gnuplot.AddDataset(dataset_delay);
+    std::ofstream plotFile(plotFileName.c_str());
+    gnuplot.GenerateOutput(plotFile);
+    plotFile.close();
+    gnuplot_DR.AddDataset(dataset_rate);
+    std::ofstream plotFileDR(plotFileNameDR.c_str());
+    gnuplot_DR.GenerateOutput(plotFileDR);
+    plotFileDR.close();
+
     Simulator::Destroy();
 
     return 0;
